2a842dec129d0de0069d5be98427cf5a3002b701
Alvin <athanasalvin@gmail.com>
2025-04-19 15:29:47 +0300

    Added gender, address, and phone to the userDTO

--- src/main/java/com/example/cms_backend/Model/DTO/UserDTO.java
+++ src/main/java/com/example/cms_backend/Model/DTO/UserDTO.java
@@ -1,6 +1,9 @@
 package com.example.cms_backend.Model.DTO;
 
 import com.example.cms_backend.Model.Entities.User;
+import com.example.cms_backend.Model.Enums.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,11 +13,17 @@ public class UserDTO {
     private Long id;
     private String name;
     private String email;
+    private String phone;
+    private String address;
+    private String gender;
 
     public UserDTO(User user) {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
+        this.phone = user.getPhone();
+        this.address = user.getAddress();
+        this.gender = user.getGender().toString();
     }
 
 }
