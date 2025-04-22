5fb12cb1b0c8852f9d37ac5a4c1f6cc2c23c94e6
Alvin <athanasalvin@gmail.com>
2025-04-22 20:14:01 +0300

    fixed adding roles issue

--- src/main/java/com/example/cms_backend/Controllers/UserRoleController.java
+++ src/main/java/com/example/cms_backend/Controllers/UserRoleController.java
@@ -4,10 +4,7 @@ import com.example.cms_backend.Model.DTO.UserDTO;
 import com.example.cms_backend.Model.UpdateCommands.AssignRoleCommand;
 import com.example.cms_backend.Roles_Authorities.UserRoles;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserRoleController {
@@ -18,7 +15,7 @@ public class UserRoleController {
     }
 
     @PostMapping("/user/role/{id}")
-    public ResponseEntity<String> assignRole(@PathVariable Long id, @RequestBody String roleName){
+    public ResponseEntity<String> assignRole(@PathVariable Long id, @RequestParam String roleName){
         return userRoles.execute(new AssignRoleCommand(id, roleName));
     }
 }
--- src/main/java/com/example/cms_backend/Roles_Authorities/UserRoles.java
+++ src/main/java/com/example/cms_backend/Roles_Authorities/UserRoles.java
@@ -1,6 +1,8 @@
 package com.example.cms_backend.Roles_Authorities;
 
 import com.example.cms_backend.Abstractions.Command;
+import com.example.cms_backend.Exceptions.RoleNotFoundException;
+import com.example.cms_backend.Exceptions.UserNotFoundException;
 import com.example.cms_backend.Model.Entities.Role;
 import com.example.cms_backend.Model.Entities.User;
 import com.example.cms_backend.Model.Enums.Roles;
@@ -40,15 +42,22 @@ public class UserRoles implements Command<AssignRoleCommand,String> {
     public ResponseEntity<String> execute(AssignRoleCommand command) {
         Optional<Role> assignedRole = roleRepository.findByName(command.getRoleName());
         Optional<User> userOptional = userRepository.findById(command.getId());
-        if (assignedRole.isPresent() && userOptional.isPresent()) {
-            Role role = assignedRole.get();
-            User user = userOptional.get();
-            if (user.getRoles() == null) {
-                user.setRoles(new HashSet<>());
+        if (assignedRole.isPresent()) {
+            if (userOptional.isPresent()) {
+                Role role = assignedRole.get();
+                User user = userOptional.get();
+                if (user.getRoles() == null) {
+                    user.setRoles(new HashSet<>());
+                }
+                user.getRoles().add(role);
+                userRepository.save(user);
+                return ResponseEntity.status(HttpStatus.CREATED).body("Role assigned: " + command.getRoleName());
+            } else {
+                throw new UserNotFoundException();
             }
-            user.getRoles().add(role);
-        }
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("Role assigned: " + command.getRoleName());
+        } else {
+            throw new RoleNotFoundException();
+        }
     }
 }
