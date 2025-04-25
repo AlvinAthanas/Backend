e9c3dd90d7d673e81af942d8f6e975d1b1633d6b
Alvin <athanasalvin@gmail.com>
2025-04-25 15:18:58 +0300

    added validation to adding multiple users

--- src/main/java/com/example/cms_backend/Services/UserServices/CreateUsersService.java
+++ src/main/java/com/example/cms_backend/Services/UserServices/CreateUsersService.java
@@ -1,9 +1,12 @@
 package com.example.cms_backend.Services.UserServices;
 
 import com.example.cms_backend.Abstractions.Command;
+import com.example.cms_backend.Exceptions.ErrorMessages;
+import com.example.cms_backend.Exceptions.UserNotValidException;
 import com.example.cms_backend.Model.DTO.UserDTO;
 import com.example.cms_backend.Model.Entities.User;
 import com.example.cms_backend.Repositories.UserRepository;
+import com.example.cms_backend.Validators.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +27,14 @@ public class CreateUsersService implements Command<List<User>,List<UserDTO>> {
 
     @Override
     public ResponseEntity<List<UserDTO>> execute(List<User> users) {
+        for (User user : users) {
+            UserValidator.validateUser(user);
+            if (!userRepository.existsByEmail(user.getEmail())) {
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
+            } else {
+                throw new UserNotValidException(ErrorMessages.EMAIL_ALREADY_EXISTS.getMessage());
+            }
+        }
         userRepository.saveAll(users);
         return ResponseEntity.status(HttpStatus.CREATED).body(users.stream().map(UserDTO::new).toList());
     }
